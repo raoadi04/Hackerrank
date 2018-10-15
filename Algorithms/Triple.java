370708ea78ee0714b72e328cbe6b9f297ac95e5e
raoadi04 <44147589+raoadi04@users.noreply.github.com>
2018-10-15 14:59:54 -0400

    Create Triple.java

---
+++ Algorithms/Triple.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) 
+    {
+        List<Integer> point  = new ArrayList<Integer>();//holds the scores for the two arrays
+        int pA=0;
+        int pB=0;
+        for(int i = 0; i < a.size() ; i++)
+        {
+            if(a.get(i)>b.get(i))
+                pA++;
+            else if(a.get(i)<b.get(i))
+                pB++;
+        }
+        
+        point.add(pA);
+        point.add(pB);
+        
+        return point;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
